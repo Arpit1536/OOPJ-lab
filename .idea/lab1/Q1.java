c898dfd9ab2c82c08fee3b9e25f7ee089215c67d
Arpit1536 <arpit10150@gmail.com>
2025-12-12 13:20:02 +0530

    lab 1

---
+++ .idea/lab1/Q1.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Q1{
+    public static void main(String[] args) {
+
+        String name = "Arpit";
+        String rollNo = "24051540";
+        String section = "CSE-33";
+        String branch = "CSE";
+
+        System.out.println("Name: " + name);
+        System.out.println("Roll No: " + rollNo);
+        System.out.println("Section: " + section);
+        System.out.println("Branch: " + branch);
+    }
+}
